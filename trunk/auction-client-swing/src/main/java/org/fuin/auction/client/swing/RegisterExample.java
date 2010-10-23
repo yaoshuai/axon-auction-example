e61efac46281dcd9cb7bae674e506a9218b1dec5
reg.google.com@future-invent.de <reg.google.com@future-invent.de@e47c3178-aff7-cc2e-030c-80264d758d81>
2010-10-23 15:48:54 +0000

    Added mail sending capability.

--- trunk/auction-client-swing/src/main/java/org/fuin/auction/client/swing/RegisterExample.java
+++ trunk/auction-client-swing/src/main/java/org/fuin/auction/client/swing/RegisterExample.java
@@ -15,6 +15,11 @@
  */
 package org.fuin.auction.client.swing;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
 import org.fuin.auction.command.api.exceptions.AuctionCmdService;
 import org.fuin.auction.command.api.exceptions.UserNameEmailCombinationAlreadyExistException;
 import org.fuin.auction.common.Utils;
@@ -22,9 +27,11 @@ import org.fuin.auction.common.Utils;
 import com.caucho.hessian.client.HessianProxyFactory;
 
 /**
- * Temporary test class to check command server connection.
+ * Quick'n dirty example usage.
  */
 public final class RegisterExample {
+
+	private static final int MINUTE = 60;
 
 	/**
 	 * Private constructor to avoid instantiation.
@@ -44,8 +51,20 @@ public final class RegisterExample {
 	 */
 	public static void main(final String[] args) throws Exception {
 
-		System.out.println("java.io.tmpdir=" + System.getProperty("java.io.tmpdir"));
+		// Display temp directory
+		final File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+		System.out.println("java.io.tmpdir=" + tmpDir);
 
+		// Delete old mail if it exists
+		final String peterEmail = "peter@nowhere.com";
+		final File mailFile = new File(tmpDir, peterEmail);
+		if (mailFile.exists()) {
+			if (!mailFile.delete()) {
+				throw new RuntimeException("Cannot delete: " + mailFile);
+			}
+		}
+
+		// Start example
 		final HessianProxyFactory factory = new HessianProxyFactory();
 
 		System.out.println("Application: "
@@ -58,21 +77,73 @@ public final class RegisterExample {
 		// Add user
 		final String peterName = "peter";
 		final String peterPw = "12345678";
-		final String peterEmail = "peter@nowhere.com";
 		final String peterId = cmdService.registerUser(peterName, peterPw, peterEmail);
-		System.out.println(peterName + "=" + peterId);
+		System.out.println("REGISTERED " + peterName + "=" + peterId);
 
-		// Error because of a duplicate user id
+		// Test duplicate registration
 		try {
 			cmdService.registerUser(peterName, peterPw, peterEmail);
 		} catch (final UserNameEmailCombinationAlreadyExistException ex) {
-			System.out.println(ex.getMessage());
+			System.out.println("REGISTERING AGAIN: " + ex.getMessage());
 		}
 
 		// Change password for peter
 		cmdService.changeUserPassword(peterId, peterPw, "abc123def");
-		System.out.println("Password for " + peterName + " changed!");
+		System.out.println("CHANGE PASSWORD: " + peterName);
 
+		// Wait for the welcome mail to arrive
+		int secondsWaited = 0;
+		while ((secondsWaited < MINUTE) && !mailFile.exists()) {
+			sleepSeconds(1);
+			secondsWaited++;
+		}
+		if (!mailFile.exists()) {
+			throw new RuntimeException("Waited more than one minute...");
+		}
+
+		final String message = readMessage(mailFile);
+		final String securityToken = extractToken(message, "?token=", "\"");
+		System.out.println("securityToken: " + securityToken);
+
+		cmdService.verifyUserEmail(peterId, securityToken);
+		System.out.println("VERFIED USER EMAIL: " + peterName);
+
+	}
+
+	private static void sleepSeconds(final int seconds) {
+		try {
+			Thread.sleep(1000 * seconds);
+		} catch (final InterruptedException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	private static String readMessage(final File file) throws IOException {
+		final StringBuffer sb = new StringBuffer();
+		final LineNumberReader reader = new LineNumberReader(new FileReader(file));
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+		} finally {
+			reader.close();
+		}
+		return sb.toString();
+	}
+
+	private static String extractToken(final String message, final String startStr,
+	        final String stopStr) {
+
+		final int start = message.indexOf(startStr);
+		if (start == -1) {
+			throw new IllegalStateException("Cannot find '" + startStr + "'!");
+		}
+		final int stop = message.indexOf(stopStr, start + 1);
+		if (stop == -1) {
+			throw new IllegalStateException("Cannot find '" + stopStr + "'!");
+		}
+		return message.substring(start + startStr.length(), stop);
 	}
 
 }
--- trunk/auction-command-server/src/main/java/org/fuin/auction/command/server/base/MailManager.java
+++ trunk/auction-command-server/src/main/java/org/fuin/auction/command/server/base/MailManager.java
@@ -15,14 +15,25 @@
  */
 package org.fuin.auction.command.server.base;
 
-import javax.inject.Named;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.velocity.app.VelocityEngine;
 import org.axonframework.eventhandling.FullConcurrencyPolicy;
 import org.axonframework.eventhandling.annotation.AsynchronousEventListener;
 import org.axonframework.eventhandling.annotation.EventHandler;
 import org.fuin.auction.command.server.events.UserCreatedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 /**
  * Responsible for sending or receiving mails.
@@ -32,6 +43,46 @@ import org.slf4j.LoggerFactory;
 public class MailManager {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MailManager.class);
+
+	@Inject
+	private JavaMailSender mailSender;
+
+	@Inject
+	private VelocityEngine velocityEngine;
+
+	@Inject
+	@Named("mailProperties")
+	private Properties mailProperties;
+
+	/**
+	 * Sets the mail sender to a new value.
+	 * 
+	 * @param mailSender
+	 *            Mail sender to set.
+	 */
+	protected final void setMailSender(final JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	/**
+	 * Sets the velocity engine to a new value.
+	 * 
+	 * @param velocityEngine
+	 *            Engine to set.
+	 */
+	protected final void setVelocityEngine(final VelocityEngine velocityEngine) {
+		this.velocityEngine = velocityEngine;
+	}
+
+	/**
+	 * Sets the mail properties.
+	 * 
+	 * @param props
+	 *            Mail properties.
+	 */
+	protected final void setMailProperties(final Properties props) {
+		this.mailProperties = props;
+	}
 
 	/**
 	 * Creates a welcome mail with a unique identifier to verify the email
@@ -43,12 +94,26 @@ public class MailManager {
 	@EventHandler
 	public final void handleUserCreatedEvent(final UserCreatedEvent event) {
 
-		// TODO michael 21.10.2010 Send a real email
-
 		if (LOG.isDebugEnabled()) {
-			LOG.debug("SEND welcome mail to " + event.getEmail() + " [" + event.getSecurityToken()
-			        + "]");
+			LOG.debug("SEND user created mail to " + event.getEmail() + " [securityToken='"
+			        + event.getSecurityToken() + "']");
 		}
+
+		final MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			public void prepare(final MimeMessage mimeMessage) throws Exception {
+				final MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+				message.setTo(event.getEmail().toString());
+				message.setFrom(mailProperties.getProperty("sender"));
+				final Map<String, String> varMap = new HashMap<String, String>();
+				varMap.put("email", event.getEmail().toString());
+				varMap.put("userName", event.getUserName().toString());
+				varMap.put("securityToken", event.getSecurityToken());
+				final String text = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine,
+				        "user-created-mail.vm", varMap);
+				message.setText(text, true);
+			}
+		};
+		this.mailSender.send(preparator);
 
 	}
 
