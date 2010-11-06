007001193a2c69a41d088dc7f43174085fbf656d
reg.google.com@future-invent.de <reg.google.com@future-invent.de@e47c3178-aff7-cc2e-030c-80264d758d81>
2010-11-06 10:58:13 +0000

    Iniitial version.

---
+++ trunk/axon-support/src/test/java/org/fuin/axon/support/base/JodaTimeConverterTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2010. Axon Auction Example
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fuin.axon.support.base;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.joda.time.LocalDateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+/**
+ * Test {@link JodaTimeConverter}.
+ * 
+ * @author Michael Schnell
+ */
+// TESTCODE:BEGIN
+public class JodaTimeConverterTest {
+
+	private JodaTimeConverter testee;
+
+	@Before
+	public final void setUp() {
+		testee = new JodaTimeConverter();
+	}
+
+	@After
+	public final void tearDown() {
+		testee = null;
+	}
+
+	@Test
+	public final void testCanConvert() {
+		assertThat(testee.canConvert(this.getClass())).isFalse();
+		assertThat(testee.canConvert(LocalDateTime.class)).isTrue();
+	}
+
+	@Test
+	public final void testMarshal() {
+
+		// PREPARE
+		final HierarchicalStreamWriter writer = mock(HierarchicalStreamWriter.class);
+		final MarshallingContext context = mock(MarshallingContext.class);
+		final LocalDateTime localDateTime = new LocalDateTime(0);
+
+		// TEST
+		testee.marshal(localDateTime, writer, context);
+
+		// ASSERT
+		verify(writer).setValue("1970-01-01T01:00:00.000");
+
+	}
+
+	@Test
+	public final void testUnmarshal() {
+
+		// PREPARE
+		final HierarchicalStreamReader reader = mock(HierarchicalStreamReader.class);
+		final UnmarshallingContext context = mock(UnmarshallingContext.class);
+		when(context.getRequiredType()).thenReturn(LocalDateTime.class);
+		when(reader.getValue()).thenReturn("1970-01-01T01:00:00.000");
+
+		// TEST
+		final Object result = testee.unmarshal(reader, context);
+
+		// ASSERT
+		assertThat(result).isEqualTo(new LocalDateTime(0));
+
+	}
+
+}
+// TESTCODE:END
